d349bf8407593d82edf98e2fd4d3f18fdd2c1582
Enealor <53704971+Enealor@users.noreply.github.com>
2020-01-24 10:58:03 -0800

    Added vector and scalar update tests

--- test_SM3.py
+++ test_SM3.py
@@ -2,31 +2,79 @@ import torch, numpy
 from SM3 import SM3, _compute_accumulator
 import pytest
 
-@pytest.mark.parametrize(
-    'lr, momentum, beta',
-    [
-        [0.1, 0.0, 0.0],
-        [0.1, 0.1, 0.0],
-        [0.1, 0.0, 0.1],
-        [0.2, 0.3, 0.1],
-        [0.3, 0.1, 0.2],
-        [0.1, 0.2, 0.3]
-    ]
-)
-def test_updates(lr, momentum, beta):
-    var = numpy.array([[.5, .5], [.5, .5]], dtype=numpy.float32)
-    grad = numpy.array([[.1, .05], [.02, .03]], dtype=numpy.float32)
 
-    x = torch.from_numpy(var).clone()
-    x.grad = torch.from_numpy(grad)
+def _test_dense_0d_updates(lr, momentum, beta):
+    var = numpy.array(0.5)
+    grad = numpy.array(0.1)
+
+    x = _create_tensor(var, grad)
+
+    opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
+
+    gbar = numpy.zeros_like(grad)
+    accumulator = numpy.zeros_like(grad)
+    for _ in range(5):
+        # Run a step of training.
+        opt.step()
+
+        # Expected preconditioned gradient, momentum, and parameter.
+        if beta > 0.:
+            accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
+        else:
+            accumulator = accumulator + numpy.square(grad)
+
+        exp_p_grad = grad / numpy.sqrt(accumulator+1e-8)
+        gbar = momentum * gbar + (1. - momentum) * exp_p_grad
+        var = var - lr * gbar
+        # Check that variable and momentum are as expected after one step of
+        # training.
+        _check_values(x.numpy(), var)
+        if momentum > 0.:
+            _check_values(opt.state[x]['momentum_buffer'].numpy(), gbar)
+
+
+def _test_dense_1d_updates(lr, momentum, beta):
+    var = numpy.array([0.5, 0.3])
+    grad = numpy.array([0.1, 0.1])
+
+    x = _create_tensor(var, grad)
+
+    opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
+
+    gbar = numpy.zeros_like(grad)
+    accumulator = numpy.zeros_like(grad)
+    for _ in range(5):
+        # Run a step of training.
+        opt.step()
+
+        # Expected preconditioned gradient, momentum, and parameter.
+        if beta > 0.:
+            accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
+        else:
+            accumulator = accumulator + numpy.square(grad)
+
+        exp_p_grad = grad / numpy.sqrt(accumulator+1e-8)
+        gbar = momentum * gbar + (1. - momentum) * exp_p_grad
+        var = var - lr * gbar
+        # Check that variable and momentum are as expected after one step of
+        # training.
+        _check_values(x.numpy(), var)
+        if momentum > 0.:
+            _check_values(opt.state[x]['momentum_buffer'].numpy(), gbar)
+
+def _test_dense_2d_updates(lr, momentum, beta):
+    var = numpy.array([[.5, .5], [.5, .5]])
+    grad = numpy.array([[.1, .05], [.02, .03]])
+
+    x = _create_tensor(var, grad)
 
     opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
 
     row_accumulator = numpy.zeros([2, 1])
     col_accumulator = numpy.zeros([1, 2])
     gbar = numpy.zeros_like(grad)
-    accumulator = numpy.zeros_like(gbar)
-    for i in range(5):
+    accumulator = numpy.zeros_like(grad)
+    for _ in range(5):
         # Run a step of training.
         opt.step()
 
@@ -44,7 +92,31 @@ def test_updates(lr, momentum, beta):
         var = var - lr * gbar
         # Check that variable and momentum are as expected after one step of
         # training.
-
-        assert numpy.isclose(x.numpy(), var).all(), 'Failed at {0}'.format(i)
+        _check_values(x.numpy(), var)
         if momentum > 0.:
-            assert numpy.isclose(opt.state[x]['momentum_buffer'].numpy(), gbar).all()
+            _check_values(opt.state[x]['momentum_buffer'].numpy(), gbar)
+
+@pytest.mark.parametrize(
+    'lr, momentum, beta',
+    [
+        [0.1, 0.0, 0.0],
+        [0.1, 0.1, 0.0],
+        [0.1, 0.0, 0.1],
+        [0.2, 0.3, 0.1],
+        [0.3, 0.1, 0.2],
+        [0.1, 0.2, 0.3]
+    ]
+)
+def test_updates(lr, momentum, beta):
+    _test_dense_0d_updates(lr, momentum, beta)
+    _test_dense_1d_updates(lr, momentum, beta)
+    _test_dense_2d_updates(lr, momentum, beta)
+
+def _check_values(var, exp_var):
+        assert numpy.isclose(var, exp_var).all()
+
+def _create_tensor(var, grad):
+    x = torch.from_numpy(var).clone()
+    x.grad = torch.from_numpy(grad)
+    # var may change independently of x
+    return x
