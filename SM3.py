b6bf350ee04675192f36aff863d6def02f57b6f7
Eleanor Dwight Holland <53704971+Enealor@users.noreply.github.com>
2020-01-31 12:24:38 -0800

    Sparse update (#2)
    Added sparse support/tests, eps param and link. Improved documentation. Enabled flake8 in CI.

--- SM3.py
+++ SM3.py
@@ -4,28 +4,36 @@ from torch.optim import Optimizer
 class SM3(Optimizer):
     """Implements SM3 algorithm.
 
-    It has been proposed in `Memory-Efficient Adaptive Optimization`__.
+    It has been proposed in `Memory-Efficient Adaptive Optimization`_.
 
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         lr (float, optional): coefficient that scale delta before it is applied
-            to the parameters (default: 1.0)
+            to the parameters (default: 0.1)
         momentum (float, optional): coefficient used to scale prior updates
-            before adding. Drastically increases memory if `momentum > 0.0` 
-            (default: 0.0)
-        beta (float, optional): coefficient used for exponential moving averages
-            (default: 0.0)
+            before adding. This drastically increases memory usage if
+            `momentum > 0.0`. This is ignored if the parameter's gradient
+            is sparse. (default: 0.0)
+        beta (float, optional): coefficient used for exponential moving
+            averages (default: 0.0)
+        eps (float, optional): Term added to square-root in denominator to
+            improve numerical stability (default: 1e-30)
+
+    .. _Memory-Efficient Adaptive Optimization:
+        https://arxiv.org/abs/1901.11150
     """
-    def __init__(self, params, lr=1.0, momentum=0.0, beta=0.0):
+    def __init__(self, params, lr=0.1, momentum=0.0, beta=0.0, eps=1e-30):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {0}".format(lr))
         if not 0.0 <= momentum < 1.0:
             raise ValueError("Invalid momentum: {0}".format(momentum))
         if not 0.0 <= beta < 1.0:
             raise ValueError("Invalid beta: {0}".format(beta))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid eps: {0}".format(eps))
 
-        defaults = dict(lr=lr, momentum=momentum, beta=beta)
+        defaults = {'lr': lr, 'momentum': momentum, 'beta': beta, 'eps': eps}
         super(SM3, self).__init__(params, defaults)
 
     def step(self, closure=None):
@@ -42,12 +50,11 @@ class SM3(Optimizer):
         for group in self.param_groups:
             momentum = group['momentum']
             beta = group['beta']
+            eps = group['eps']
             for p in group['params']:
                 if p is None:
                     continue
                 grad = p.grad.data
-                if grad.is_sparse:
-                    raise NotImplementedError
 
                 state = self.state[p]
                 shape = grad.shape
@@ -57,66 +64,117 @@ class SM3(Optimizer):
                 if len(state) == 0:
                     state['step'] = 0
                     state['momentum_buffer'] = 0.
-                    _add_zero_acc(state, grad.shape, grad.dtype, grad.device)
+                    _add_initial_accumulators(state, grad)
 
-                # Get previous accumulators mu_{t-1}
-                if rank > 1:
-                    acc_list = [state[_key(i)] for i in range(rank)]
+                if grad.is_sparse:
+                    # the update is non-linear so indices must be unique
+                    grad.coalesce()
+                    grad_indices = grad._indices()
+                    grad_values = grad._values()
+
+                    # Transform update_values into sparse tensor
+                    def make_sparse(values):
+                        constructor = grad.new
+                        if grad_indices.dim() == 0 or values.dim() == 0:
+                            return constructor().resize_as_(grad)
+                        return constructor(grad_indices, values, grad.size())
+
+                    acc = state[_key(0)]
+                    update_values = _compute_sparse_update(beta, acc, grad_values, grad_indices)
+
+                    self._update_sparse_accumulator(beta, acc, make_sparse(update_values))
+
+                    # Add small amount for numerical stability
+                    update_values.add_(eps).rsqrt_().mul_(grad_values)
+
+                    update = make_sparse(update_values)
                 else:
-                    acc_list = [state[_key(0)]]
+                    # Get previous accumulators mu_{t-1}
+                    if rank > 1:
+                        acc_list = [state[_key(i)] for i in range(rank)]
+                    else:
+                        acc_list = [state[_key(0)]]
 
-                # Get update from accumulators
-                update = _compute_accumulator(acc_list, shape)
-                if beta > 0.:
-                    update.mul_(beta)
-                update.addcmul_(1. - beta, grad, grad)
+                    # Get update from accumulators and gradients
+                    update = _compute_update(beta, acc_list, grad)
 
-                # Update accumulators
-                for i, acc in enumerate(acc_list):
-                    nu_max = _max_reduce_except_dim(update, i)
-                    torch.max(acc, nu_max, out=acc)
+                    # Update accumulators.
+                    self._update_accumulator(beta, acc_list, update)
 
-                # Add small amount for numerical stability
-                # TODO: Add eps argument
-                update.add_(1e-30).rsqrt_().mul_(grad)
+                    # Add small amount for numerical stability
+                    update.add_(eps).rsqrt_().mul_(grad)
 
-                if momentum > 0.:
-                    m = state['momentum_buffer']
-                    update.mul_(1. - momentum).add_(momentum, m)
-                    state['momentum_buffer'] = update.detach()
+                    if momentum > 0.:
+                        m = state['momentum_buffer']
+                        update.mul_(1. - momentum).add_(momentum, m)
+                        state['momentum_buffer'] = update.detach()
 
                 p.data.sub_(group['lr'], update)
                 state['step'] += 1
         return loss
 
-def _key(i):
-    # Returns key used for accessing accumulators
-    return 'accumulator_' + str(i)
+    def _update_accumulator(self, beta, acc_list, update):
+        for i, acc in enumerate(acc_list):
+            nu_max = _max_reduce_except_dim(update, i)
+            if beta > 0.:
+                torch.max(acc, nu_max, out=acc)
+            else:
+                # No need to compare - nu_max is bigger because of grad ** 2
+                acc.copy_(nu_max)
 
-def _compute_accumulator(accumulators, shape):
-    # Gets the minimum across the accumulators.
-    rank = len(shape)
-    result = accumulators[0].clone()
+    def _update_sparse_accumulator(self, beta, acc, update):
+        nu_max = _max_reduce_except_dim(update.to_dense(), 0).squeeze()
+        if beta > 0.:
+            torch.max(acc, nu_max, out=acc)
+        else:
+            # No need to compare - nu_max is bigger because of grad ** 2
+            acc.copy_(nu_max)
+
+def _compute_sparse_update(beta, acc, grad_values, grad_indices):
+    # In the sparse case, a single accumulator is used.
+    update_values = torch.gather(acc, 0, grad_indices[0])
+    if beta > 0.:
+        update_values.mul_(beta)
+    update_values.addcmul_(1. - beta, grad_values, grad_values)
+    return update_values
+
+def _compute_update(beta, acc_list, grad):
+    rank = len(acc_list)
+    update = acc_list[0].clone()
     for i in range(1, rank):
         # We rely on broadcasting to get the proper end shape.
         # Note that torch.min is currently (as of 1.23.2020) not commutative -
         # the order matters for NaN values.
-        result = torch.min(result, accumulators[i])
-    return result
+        update = torch.min(update, acc_list[i])
+    if beta > 0.:
+        update.mul_(beta)
+    update.addcmul_(1. - beta, grad, grad)
 
-def _add_zero_acc(state, shape, dtype, device):
-    # Creates initial accumulator
+    return update
+
+def _key(i):
+    # Returns key used for accessing accumulators
+    return 'accumulator_' + str(i)
+
+def _add_initial_accumulators(state, grad):
+    # Creates initial accumulators. For a dense tensor of shape (n1, n2, n3),
+    # then our initial accumulators are of shape (n1, 1, 1), (1, n2, 1) and
+    # (1, 1, n3). For a sparse tensor of shape (n, *), we use a single
+    # accumulator of shape (n,).
+    shape = grad.shape
     rank = len(shape)
+    defaults = {'device': grad.device, 'dtype': grad.dtype}
     acc = {}
-    if rank == 0:
-        # We handle the scalar case separately.
-        # TODO: Add memory_format=torch.preserve_format (new in PyTorch 1.4)
-        acc[_key(0)] = torch.zeros(shape, dtype=dtype, device=device)
+
+    if grad.is_sparse:
+        acc[_key(0)] = torch.zeros(shape[0], **defaults)
+    elif rank == 0:
+        # The scalar case is handled separately
+        acc[_key(0)] = torch.zeros(shape, **defaults)
     else:
         for i in range(rank):
-            acc_shape = [1]*i + [shape[i]] + [1]*(rank-1-i)
-            # TODO: Add memory_format=torch.preserve_format (new in PyTorch 1.4)
-            acc[_key(i)] = torch.zeros(acc_shape, dtype=dtype, device=device)
+            acc_shape = [1] * i + [shape[i]] + [1] * (rank - 1 - i)
+            acc[_key(i)] = torch.zeros(acc_shape, **defaults)
 
     state.update(acc)
 
--- test_SM3.py
+++ test_SM3.py
@@ -1,6 +1,7 @@
-import torch, numpy
-from SM3 import SM3, _compute_accumulator
+import torch
+import numpy
 import pytest
+from SM3 import SM3
 
 
 def _test_dense_0d_updates(lr, momentum, beta):
@@ -10,14 +11,12 @@ def _test_dense_0d_updates(lr, momentum, beta):
     x = _create_tensor(var, grad)
 
     opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
-
     gbar = numpy.zeros_like(grad)
     accumulator = numpy.zeros_like(grad)
     for _ in range(5):
         # Run a step of training.
         opt.step()
 
-        # Expected preconditioned gradient, momentum, and parameter.
         if beta > 0.:
             accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
         else:
@@ -47,7 +46,6 @@ def _test_dense_1d_updates(lr, momentum, beta):
         # Run a step of training.
         opt.step()
 
-        # Expected preconditioned gradient, momentum, and parameter.
         if beta > 0.:
             accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
         else:
@@ -97,6 +95,51 @@ def _test_dense_2d_updates(lr, momentum, beta):
             _check_values(opt.state[x]['momentum_buffer'].numpy(), gbar)
 
 @pytest.mark.parametrize(
+    'lr, beta',
+    [
+        [0.1, 0.0],
+        [0.1, 0.1],
+        [0.2, 0.3],
+        [0.3, 0.1],
+        [0.1, 0.9]
+    ]
+)
+def test_sparse_updates(lr, beta):
+    var = numpy.array([[0.5, 0.05], [0.05, 1.0], [0.15, 3.0], [0.35, 2.0]])
+    # A sparse gradient that updates index 1, and 3.
+    indices = [1, 3]
+    grad_values = numpy.array([0.1, 0.05, 0.01, 1.5])
+    grad_indices = torch.LongTensor([[1, 1, 3, 3], [0, 1, 0, 1]])
+    shape = torch.Size(var.shape)
+    sparse_grad = torch.sparse.FloatTensor(
+        grad_indices,
+        torch.from_numpy(grad_values),
+        shape
+    )
+
+    x = _create_tensor(var, sparse_grad)
+    opt = SM3([x], lr=lr, beta=beta)
+    row_accumulator = numpy.zeros([4, 1])
+
+    for _ in range(5):
+        opt.step()
+
+        accumulator = numpy.repeat(row_accumulator, 2, 1)
+        if beta > 0.:
+            accumulator[indices, :] = beta * accumulator[indices, :] \
+                + (1. - beta) * numpy.square(grad_values.reshape(2, 2))
+        else:
+            accumulator[indices, :] += numpy.square(grad_values.reshape(2, 2))
+
+        row_accumulator = numpy.amax(accumulator, axis=1, keepdims=True)
+        # Update SM3 accumulators.
+        exp_p_grad_values = grad_values.reshape(2, 2) / numpy.sqrt(accumulator[indices, :])
+        var[indices, :] = var[indices, :] - lr * exp_p_grad_values
+
+        _check_values(x.numpy()[indices, :], var[indices, :])
+        _check_values(opt.state[x]['accumulator_0'].numpy().flatten(), row_accumulator.flatten())
+
+@pytest.mark.parametrize(
     'lr, momentum, beta',
     [
         [0.1, 0.0, 0.0],
@@ -107,16 +150,20 @@ def _test_dense_2d_updates(lr, momentum, beta):
         [0.1, 0.2, 0.3]
     ]
 )
-def test_updates(lr, momentum, beta):
+def test_dense_updates(lr, momentum, beta):
     _test_dense_0d_updates(lr, momentum, beta)
     _test_dense_1d_updates(lr, momentum, beta)
     _test_dense_2d_updates(lr, momentum, beta)
 
+
 def _check_values(var, exp_var):
-        assert numpy.isclose(var, exp_var).all()
+    assert numpy.isclose(var, exp_var).all()
 
 def _create_tensor(var, grad):
+    # var may change independently of x, so clone is necessary.
     x = torch.from_numpy(var).clone()
-    x.grad = torch.from_numpy(grad)
-    # var may change independently of x
+    if isinstance(grad, torch.Tensor):
+        x.grad = grad
+    else:
+        x.grad = torch.from_numpy(grad)
     return x
