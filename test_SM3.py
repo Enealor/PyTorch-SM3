9e593f10683df1cf28bca92399ce1a1ac41a4398
Enealor <53704971+Enealor@users.noreply.github.com>
2020-01-30 19:43:25 -0800

    Added sparse support/tests, eps param and link

--- test_SM3.py
+++ test_SM3.py
@@ -1,5 +1,5 @@
 import torch, numpy
-from SM3 import SM3, _compute_accumulator
+from SM3 import SM3
 import pytest
 
 
@@ -9,7 +9,7 @@ def _test_dense_0d_updates(lr, momentum, beta):
 
     x = _create_tensor(var, grad)
 
-    opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
+    opt = SM3([x], lr=lr, momentum=momentum, beta=beta, eps=1e-30)
 
     gbar = numpy.zeros_like(grad)
     accumulator = numpy.zeros_like(grad)
@@ -17,7 +17,6 @@ def _test_dense_0d_updates(lr, momentum, beta):
         # Run a step of training.
         opt.step()
 
-        # Expected preconditioned gradient, momentum, and parameter.
         if beta > 0.:
             accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
         else:
@@ -39,7 +38,7 @@ def _test_dense_1d_updates(lr, momentum, beta):
 
     x = _create_tensor(var, grad)
 
-    opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
+    opt = SM3([x], lr=lr, momentum=momentum, beta=beta, eps=1e-30)
 
     gbar = numpy.zeros_like(grad)
     accumulator = numpy.zeros_like(grad)
@@ -47,7 +46,6 @@ def _test_dense_1d_updates(lr, momentum, beta):
         # Run a step of training.
         opt.step()
 
-        # Expected preconditioned gradient, momentum, and parameter.
         if beta > 0.:
             accumulator = beta * accumulator + (1. - beta) * numpy.square(grad)
         else:
@@ -68,7 +66,7 @@ def _test_dense_2d_updates(lr, momentum, beta):
 
     x = _create_tensor(var, grad)
 
-    opt = SM3([x], lr=lr, momentum=momentum, beta=beta)
+    opt = SM3([x], lr=lr, momentum=momentum, beta=beta, eps=1e-30)
 
     row_accumulator = numpy.zeros([2, 1])
     col_accumulator = numpy.zeros([1, 2])
@@ -96,6 +94,42 @@ def _test_dense_2d_updates(lr, momentum, beta):
         if momentum > 0.:
             _check_values(opt.state[x]['momentum_buffer'].numpy(), gbar)
 
+def _test_sparse_updates(lr, beta):
+    var = numpy.array([[0.5, 0.05], [0.05, 1.0], [0.15, 3.0], [0.35, 2.0]])
+    # A sparse gradient that updates index 1, and 3.
+    grad = numpy.array([[0.1, 0.05], [0.01, 1.5]])
+    indices = [1, 3]
+    grad_values = numpy.array([0.1, 0.05, 0.01, 1.5])
+    grad_indices = torch.LongTensor([[1, 1, 3, 3], [0, 1, 0, 1]])
+    shape = torch.Size(var.shape)
+    sparse_grad = torch.sparse.FloatTensor(
+        grad_indices,
+        torch.from_numpy(grad_values),
+        shape
+    )
+
+    x = _create_tensor(var, sparse_grad)
+    opt = SM3([x], lr=lr, beta=beta, eps=1e-30)
+    row_accumulator = numpy.zeros([4, 1])
+
+    for _ in range(5):
+        opt.step()
+
+        accumulator = numpy.repeat(row_accumulator, 2, 1)
+        if beta > 0.:
+            accumulator[indices, :] = beta * accumulator[indices, :] \
+                                    + (1. - beta) * numpy.square(grad_values.reshape(2, 2))
+        else:
+            accumulator[indices, :] += numpy.square(grad_values.reshape(2, 2))
+
+        row_accumulator = numpy.amax(accumulator, axis=1, keepdims=True)
+        # Update SM3 accumulators.
+        exp_p_grad_values = grad_values.reshape(2, 2) / numpy.sqrt(accumulator[indices, :])
+        var[indices, :] = var[indices, :] - lr * exp_p_grad_values
+
+        _check_values(x.numpy()[indices, :], var[indices, :])
+        _check_values(opt.state[x]['accumulator_0'].numpy().flatten(), row_accumulator.flatten())
+
 @pytest.mark.parametrize(
     'lr, momentum, beta',
     [
@@ -111,12 +145,16 @@ def test_updates(lr, momentum, beta):
     _test_dense_0d_updates(lr, momentum, beta)
     _test_dense_1d_updates(lr, momentum, beta)
     _test_dense_2d_updates(lr, momentum, beta)
+    _test_sparse_updates(lr, beta)
 
 def _check_values(var, exp_var):
         assert numpy.isclose(var, exp_var).all()
 
 def _create_tensor(var, grad):
+    # var may change independently of x, so clone is necessary.
     x = torch.from_numpy(var).clone()
-    x.grad = torch.from_numpy(grad)
-    # var may change independently of x
+    if isinstance(grad, torch.Tensor):
+        x.grad = grad
+    else:
+        x.grad = torch.from_numpy(grad)
     return x
